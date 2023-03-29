50353b6d30c1c18761d4a9d371abe31233971ecb
BerkayBuyukbas <122968469+BerkayBuyukbas@users.noreply.github.com>
2023-03-29 15:03:12 +0300

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,7 +1,7 @@
 <?php
 $databaseHost = 'localhost';
-$databaseName = 'id20526921_myadmin';
-$databaseUsername = 'id20526921_berkaybuyukbas';
+$databaseName = 'id20526921_berkaybuyukbas';
+$databaseUsername = 'id20526921_myadmin';
 $databasePassword = 'eK6afFqi';
 
 $baglan = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
